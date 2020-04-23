899c4917070844c8c746a710557821e5fbaa6e8f
Stefan <stefan.w0718@gmail.com>
2020-04-23 18:45:19 +1000

    solved Coles cookie requirement problem

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const routes = require('./routes/index');
-const bodyparser = require('body-parser');
-
 const app = express();
+const bodyparser = require('body-parser');
 
 app.set('view engine', 'pug');
 app.use(bodyparser.urlencoded({extended: true}));
-app.use('/', routes);
 app.use(express.static('public'));
+
+app.use('/', require('./routes/index'));
 
 const server = app.listen(7180, () => {
     console.log(`Server is running on port ${server.address().port}`);
--- public/js/script.js
+++ public/js/script.js
@@ -3,20 +3,33 @@ window.addEventListener('load', () => {
     var input = document.getElementById('query');
     var invalidFeedback = document.getElementsByClassName('invalid-feedback');
     form.addEventListener('submit', e => {
+        e.preventDefault();
+        e.stopPropagation();
         // only allow digits, letters and middle white space
-        if (!input.value.trim().match(/^[0-9a-zA-Z\s]+$/)){
-            e.preventDefault();
-            e.stopPropagation();
-            invalidFeedback[0].style.display = "block";
-            invalidFeedback[0].classList.remove("fade-out");
-            invalidFeedback[0].classList.add("fade-in");
+        if (!input.value.trim().match(/^[0-9a-zA-Z\s]+$/)) {
+            invalidFeedback[0].style.display = 'block';
+            invalidFeedback[0].classList.remove('fade-out');
+            invalidFeedback[0].classList.add('fade-in');
         }
-        else if (invalidFeedback[0].style.display === "block"){
-            invalidFeedback[0].classList.remove("fade-in");
-            invalidFeedback[0].classList.add("fade-out");
-            setTimeout(() => {
-                invalidFeedback[0].style.display = "none";
-            }, 1000);
+        else {
+            $.ajax({
+                type: 'POST',
+                url: '/',
+                data: {
+                    "query": input.value.trim(),
+                    "userAgent": navigator.userAgent
+                }
+            }).done((data) => {
+                console.log(data);
+            });
+            if (invalidFeedback[0].style.display === 'block') {
+                invalidFeedback[0].classList.remove('fade-in');
+                invalidFeedback[0].classList.add('fade-out');
+                setTimeout(() => {
+                    invalidFeedback[0].style.display = 'none';
+                }, 1000);
+            }
         }
     }, false);
 }, false);
+
