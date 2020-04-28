4277cc49531fb4811cd154156288f0c531472442
Stefan <stefan.w0718@gmail.com>
2020-04-28 23:29:16 +1000

    more progress on item list

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,72 @@
-window.addEventListener('load', () => {
-    var form = document.querySelector('.needs-validation');
+function postData() {  
     var input = document.getElementById('query');
-    var invalidFeedback = document.getElementsByClassName('invalid-feedback');
+    // only allow digits, letters and middle white space
+    if (!input.value.trim().match(/^[0-9a-zA-Z\s]+$/)) {
+        $('.invalid-feedback').fadeIn();
+    }
+    else {
+        $('.invalid-feedback').fadeOut();
+        $('#background').animate( {opacity: 0.3} );
+        $('.loading').fadeIn();
+        $.ajax({
+            type: 'POST',
+            url: '/',
+            data: {
+                "query": input.value.trim(),
+                "userAgent": navigator.userAgent
+            }
+        }).done((data) => {
+            $('.loading').fadeOut();
+            var listColes = $('#listColes');
+            var urlColes = "https://shop.coles.com.au";
+            var listWoolworths = $('#listWoolworths');
+            console.log(data);
+            createList(data.fromColes, listColes, urlColes, input.value.trim());
+        });
+    }
+}
+
+function createList(data, listgroup, url, itemId) {
+    switch (data.length) {
+        case 0:
+            break;
+        default:
+            itemId = itemId.replace(/\s/g, '-');
+            var listItem = listgroup.children().clone();
+            var delayTime = 500;
+            listgroup.fadeIn();
+            for (var i = 0; i < data.length; i++) {
+                listItem.attr('id', itemId + '-' + i);
+                listItem.find('img').attr('src', url + data[i].itemImage);
+                listItem.find('#item-name-size').text(data[i].itemBrand + ' ' + data[i].itemName + ' ' + data[i].itemSize);
+                listItem.find('small').text(data[i].itemPackagePrice);
+                if (data[i].itemSaving !== 0) {
+                    listItem.find('h5#item-saving').show();
+                    listItem.find('b#item-saving').text('You will save $' + data[i].itemSaving + '!');
+                }
+                else listItem.find('h5#item-saving').hide();
+                if (data[i].itemPromoQty !== 0) {
+                    listItem.find('h5#item-promo').show();
+                    listItem.find('b#item-promo').text('You will save $' + (((data[i].itemDollar + data[i].itemCent) * data[i].itemPromoQty) - data[i].itemPromoPrice) + ' if you buy ' + data[i].itemPromoQty + '!');
+                }
+                else listItem.find('h5#item-promo').hide();
+                listItem.find('b#dollar').text(data[i].itemDollar);
+                listItem.find('#cent').text((data[i].itemCent === 0) ? '' : data[i].itemCent * 100);
+                listgroup.append(listItem.clone().delay(delayTime += 100).fadeTo(400, 1));
+            }
+            return listgroup;
+    }   
+    console.log(listgroup.html());
+}
+
+window.addEventListener('load', () => {
+    $('#background').fadeIn();
+    $('#searchBar').fadeIn();
+    var form = document.querySelector('.needs-validation');
     form.addEventListener('submit', e => {
         e.preventDefault();
         e.stopPropagation();
-        // only allow digits, letters and middle white space
-        if (!input.value.trim().match(/^[0-9a-zA-Z\s]+$/)) {
-            invalidFeedback[0].style.display = 'block';
-            invalidFeedback[0].classList.remove('fade-out');
-            invalidFeedback[0].classList.add('fade-in');
-        }
-        else {
-            $.ajax({
-                type: 'POST',
-                url: '/',
-                data: {
-                    "query": input.value.trim(),
-                    "userAgent": navigator.userAgent
-                }
-            }).done((data) => {
-                console.log(data);
-            });
-            if (invalidFeedback[0].style.display === 'block') {
-                invalidFeedback[0].classList.remove('fade-in');
-                invalidFeedback[0].classList.add('fade-out');
-                setTimeout(() => {
-                    invalidFeedback[0].style.display = 'none';
-                }, 1000);
-            }
-        }
+        postData();
     }, false);
 }, false);
 
--- routes/index.js
+++ routes/index.js
@@ -16,32 +16,50 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    (async () => {
-        const browser = await puppeteer.launch();
-        const page_coles = await browser.newPage();
-        await page_coles.setUserAgent(req.body.userAgent);
-        await page_coles.evaluateOnNewDocument(() => {
-            Object.defineProperty(navigator, 'webdriver', {
-                get: () => undefined,
-            });
-        });
-        await page_coles.goto(url_coles + req.body.query);
-        // wait until page full loaded
-        await page_coles.waitForNavigation({
-            waitUntil: 'networkidle0',
-        });
-        const metadata = await page_coles.evaluate(() => {
-            const results = {};
-            document.querySelectorAll('span.product-name').forEach((item, i) => {
-                results[i] = { 'ProductName': item.innerText };
-            });
-            return results;
-        });
-        console.log(metadata);
-        res.send(metadata);
-        await browser.close();
-    })();
+    // (async () => {
+    //     const browser = await puppeteer.launch();
+    //     const page_coles = await browser.newPage();
+    //     await page_coles.setUserAgent(req.body.userAgent);
+    //     await page_coles.evaluateOnNewDocument(() => {
+    //         Object.defineProperty(navigator, 'webdriver', {
+    //             get: () => undefined,
+    //         });
+    //     });
+    //     await page_coles.goto(url_coles + req.body.query);
+    //     // wait until page full loaded
+    //     await page_coles.waitForNavigation({
+    //         waitUntil: 'networkidle0',
+    //     });
+    //     const metadata = await page_coles.evaluate(() => {
+    //         const results = {};
+    //         results['fromColes'] = [];
+    //         const itemMain = document.querySelectorAll('header.product-header');
+    //         itemMain.forEach((item, i) => {
+    //             results['fromColes'].push({
+    //                 "itemId": i,
+    //                 "itemImage": item.querySelector('a.product-image-link img').getAttribute('src'),
+    //                 "itemBrand": item.querySelector('span.product-brand').innerText,
+    //                 "itemName": item.querySelector('span.product-name').innerText,
+    //                 "itemDollar": parseInt(item.querySelector('span.dollar-value').innerText),
+    //                 "itemCent": parseFloat(item.querySelector('span.cent-value').innerText),
+    //                 "itemSize": item.querySelector('span.package-size').innerText,
+    //                 "itemPackagePrice": item.querySelector('span.package-price').innerText,
+    //                 "itemPromoQty": (item.querySelector('.icon-discount') !== null) ? parseInt(item.querySelector('span.promo-txt-line1').innerText.charAt(0)) : 0,
+    //                 "itemPromoPrice": (item.querySelector('.icon-discount') !== null) ? parseFloat(item.querySelector('span.promo-txt-line2').innerText.replace('$', '')) : 0,
+    //                 "itemSaving": (item.querySelector('span.saving-container') !== null) ? parseFloat(item.querySelector('.product-save-value').innerText.replace('$', '')) : 0
+    //             });
+    //         });
+    //         return results;
+    //     });
+    //     res.send(metadata);
+    //     await browser.close();
+    // })();
+
+    const fs = require('fs');
+    fs.readFile('fromColes.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        res.send(JSON.parse(data));
+    });
 });
 
 module.exports = router;
-
