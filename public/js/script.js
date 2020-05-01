f8d8c87f8d3ffb23204612fffa432f45bf47be9a
Stefan <stefan.w0718@gmail.com>
2020-05-01 19:45:03 +1000

    fixed bugs and will start ww data fetch

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,47 @@
-function postData() {  
-    var input = document.getElementById('query');
-    // only allow digits, letters and middle white space
-    if (!input.value.trim().match(/^[0-9a-zA-Z\s]+$/)) {
-        $('.invalid-feedback').fadeIn();
-    }
-    else {
-        $('.invalid-feedback').fadeOut();
-        $('#background').animate( {opacity: 0.3} );
-        $('.loading').fadeIn();
-        $.ajax({
-            type: 'POST',
-            url: '/',
-            data: {
-                "query": input.value.trim(),
-                "userAgent": navigator.userAgent
-            }
-        }).done((data) => {
-            $('.loading').fadeOut();
-            var listColes = $('#listColes');
-            var urlColes = "https://shop.coles.com.au";
-            var listWoolworths = $('#listWoolworths');
-            console.log(data);
-            createList(data.fromColes, listColes, urlColes);
-        });
-    }
+// store all user chosen data here
+var storedData = {};
+
+window.addEventListener('load', () => {
+    $('#background').fadeIn();
+    $('#searchBar').fadeIn();
+    var form = document.querySelector('.needs-validation');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        e.stopPropagation();
+        // only allow digits, letters and middle white space
+        if (!$('#query')[0].value.trim().match(/^[0-9a-zA-Z\s]+$/)) {
+            $('.invalid-feedback').fadeIn();
+        }
+        else {
+            $('.invalid-feedback').fadeOut();
+            $('#background').animate( {opacity: 0.3} );
+            $('.loading').fadeIn();
+            $('a.list-group-item').remove('.activeListItem');
+            fetchFromColes();
+        }
+    }, false);
+}, false);
+
+// Coles uses Cookie fingerprint and no API so have to fetch from serverside using Puppeteer
+function fetchFromColes() {
+    var urlColes = "https://shop.coles.com.au";
+    $.ajax({
+        type: 'POST',
+        url: '/',
+        data: {
+            "query": $('#query')[0].value.trim(),
+            "userAgent": navigator.userAgent
+        }
+    }).done((data) => {
+        $('.loading').fadeOut();
+        console.log(data);
+        createList(data.fromColes, $('#listColes'), urlColes);
+    });
+}
+
+// Woolies provides API so data can be fetched from clientside
+function fetchFromWoolies() {
+    var urlWoolies = 'https://www.woolworths.com.au/shop/search/products?searchTerm=';
 }
 
 // create search result
@@ -58,8 +76,27 @@ function createList(data, listgroup, url) {
                 listgroup.append(listItem.clone().delay(delayTime += 100).fadeTo(400, 1));
             }
             addToShoppingList(listgroup, data);
-            return listgroup;
     }
+}
+
+// create shopping list with animation
+function addToShoppingList(listgroup, data) {
+    listgroup.unbind(); // It's necessary to unbind all handlers before binding new onclick functions
+    listgroup.on('click', 'a.list-group-item', (e) => {
+        var chosenItem = $(e.currentTarget).find('img').clone().removeClass('border-0').addClass('rounded-circle chosen-item');
+        var chosenData = data[$('.activeListItem').index($(e.currentTarget))];
+        var vendorCode = '';
+        if (listgroup.attr('id') === 'listColes') {
+            chosenItem.css('box-shadow', 'inset 0 0 0 2vmax #de1f27');
+            vendorCode = 'co'; // Coles
+        }
+        else {
+            chosenItem.css('box-shadow', 'inset 0 0 0 2vmax #178841');
+            vendorCode = 'ww'; // Woolies
+        }
+        var latestAdded = storeChosenItems(chosenData, vendorCode);
+        addToShoppingListAnimation(chosenItem, e.currentTarget, latestAdded);
+    });
 }
 
 // store user-chosen items data and return json
@@ -72,25 +109,6 @@ function storeChosenItems(data, from) {
         storedData[data.itemSKU].order = Object.keys(storedData).length++;
     }
     return storedData[data.itemSKU];
-}
-
-// create shopping list with animation
-function addToShoppingList(listgroup, data) {
-    listgroup.on('click', 'a.list-group-item', (e) => {
-        var chosenItem = $(e.currentTarget).find('img').clone().removeClass('border-0').addClass('rounded-circle chosen-item');
-        var chosenData = data[$('.activeListItem').index($(e.currentTarget))];
-        var vendorCode = '';
-        if (listgroup.attr('id') === 'listColes') {
-            chosenItem.css('box-shadow', 'inset 0 0 0 2vmax #de1f27');
-            vendorCode = 'co'; // Coles
-        }
-        else {
-            chosenItem.css('box-shadow', 'inset 0 0 0 2vmax #178841');
-            vendorCode = 'ww'; // Woolworths
-        }
-        var latestAdded = storeChosenItems(chosenData, vendorCode);
-        addToShoppingListAnimation(chosenItem, e.currentTarget, latestAdded);
-    });
 }
 
 function addToShoppingListAnimation (chosenItem, parentList, latestAdded) {
@@ -110,7 +128,6 @@ function addToShoppingListAnimation (chosenItem, parentList, latestAdded) {
         container.animate({ 'height': '7vmax'}, 1000);
     }
     var latestContainer = shoppingList.find('#container-' + latestAdded.order);
-    console.log(latestAdded.order);
     animated.appendTo(latestContainer).animate({
         'top': latestContainer.offset().top,
         'left': latestContainer.offset().left,
@@ -118,17 +135,3 @@ function addToShoppingListAnimation (chosenItem, parentList, latestAdded) {
     }, 1000, 'swing');
     chosenItem.delay(1000).fadeIn();
 }
-
-window.addEventListener('load', () => {
-    $('#background').fadeIn();
-    $('#searchBar').fadeIn();
-    var form = document.querySelector('.needs-validation');
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        e.stopPropagation();
-        postData();
-    }, false);
-}, false);
-
-// store all user chosen data here
-var storedData = {};
